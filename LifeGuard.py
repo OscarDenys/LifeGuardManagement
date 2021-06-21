9d646793fd550f01bb2a759345317c9e4042e9d7
oscar denys <oscar.denys@student.kuleuven.be>
2021-06-21 14:39:37 +0200

    changed input to txt files

--- LifeGuard.py
+++ LifeGuard.py
@@ -3,22 +3,30 @@ import os
 import math
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-correct_ratings_input_path = os.path.join(THIS_FOLDER, 'CorrectRatingsInput.csv')
+matrix1_path = os.path.join(THIS_FOLDER, '1.txt')
+matrix2_path = os.path.join(THIS_FOLDER, '2.txt')
+matrix3_path = os.path.join(THIS_FOLDER, '3.txt')
+matrix4_path = os.path.join(THIS_FOLDER, '4.txt')
+matrix5_path = os.path.join(THIS_FOLDER, '5.txt')
+matrix6_path = os.path.join(THIS_FOLDER, '6.txt')
+matrix7_path = os.path.join(THIS_FOLDER, '7.txt')
+matrix8_path = os.path.join(THIS_FOLDER, '8.txt')
+matrix9_path = os.path.join(THIS_FOLDER, '9.txt')
+matrix10_path = os.path.join(THIS_FOLDER, '10.txt')
 
-f = open("1.in", "r")
-numbers1 = []
-nb_pairs1 = 0
-i = 0
-for num in f.read():
-    if num != '\n' and num != ' ':
-        if i == 0:
-            nb_pairs1 = int(num)
-        else:
-            numbers1.append(int(num))
-        i += 1
+# load matrices
+matrix1 = np.loadtxt(matrix1_path, dtype=int)
+matrix2 = np.loadtxt(matrix2_path, dtype=int)
+matrix3 = np.loadtxt(matrix3_path, dtype=int)
+matrix4 = np.loadtxt(matrix4_path, dtype=int)
+matrix5 = np.loadtxt(matrix5_path, dtype=int)
+matrix6 = np.loadtxt(matrix6_path, dtype=int)
+matrix7 = np.loadtxt(matrix7_path, dtype=int)
+matrix8 = np.loadtxt(matrix8_path, dtype=int)
+matrix9 = np.loadtxt(matrix9_path, dtype=int)
+matrix10 = np.loadtxt(matrix10_path, dtype=int)
 
-matrix1 = np.array(numbers1)
-matrix1 = matrix1.reshape((nb_pairs1, 2))
+print('matrices are loaded')
 
 
 def find_next_a(mat, a, loss):
@@ -45,7 +53,7 @@ def fire_lifeguard(mat):
     covered_time = 0
     b = max(mat[:, 1])
     a = mat[np.argmax(mat[:, 1]), 0]
-    print(mat)
+    # print(mat)
     while mat.size != 0:
         find_next_a(mat, a, loss)
         covered_time += (b - a)
@@ -53,7 +61,10 @@ def fire_lifeguard(mat):
         if mat.size != 0:
             b = max(mat[:, 1])
             a = mat[np.argmax(mat[:, 1]), 0]
-            print(mat)
+            # print(mat)
+
+    print(covered_time)
+    print(min(loss))
 
 
 
@@ -62,13 +73,7 @@ def fire_lifeguard(mat):
 
 
 
-
-
-
-
-
-
-fire_lifeguard(matrix1)
+fire_lifeguard(c)
 
 
 
